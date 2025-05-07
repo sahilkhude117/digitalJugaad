906b0ec2530f95cd555c6578a0386844caf0167d
sahilkhude117 <sahilkhude11@gmail.com>
2025-05-07 16:21:24 +0530

    landing: mock landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import BestSellers from "@/components/sections/BestSellers";
 
-export default function Home() {
+import { Navbar } from "@/components/sections/Navbar"
+import HeroSection from "@/components/sections/Hero"
+import { HowItWorks } from "@/components/sections/HowItWorks"
+import { Testimonials } from "@/components/sections/Testimonials"
+import { FAQSection } from "@/components/sections/FAQ"
+import { Footer } from "@/components/sections/Footer"
+import BestSellers from "@/components/sections/BestSellers"
+
+export default function LandingPage() {
+
   return (
-    <main className="bg-white">
-      <BestSellers />
-    </main>
-  );
+    <div className="flex min-h-[100dvh] flex-col">
+      <Navbar/>
+      <main className="flex-1">
+        <HeroSection/>
+        <BestSellers/>
+
+        {/* How It Works Section */}
+        <HowItWorks/>
+
+        {/* Testimonials Section */}
+        <Testimonials/>
+
+        {/* FAQ Section */}
+        <FAQSection/>
+
+      </main>
+      <Footer/>
+    </div>
+  )
 }
+
